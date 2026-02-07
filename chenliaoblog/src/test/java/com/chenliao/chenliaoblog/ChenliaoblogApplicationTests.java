f87191b56b414d30d55440b71f959f80bcbaf7cf
cjw <cjw18476714119@163.com>
2026-02-07 11:36:22 +0800

    初始

---
+++ chenliaoblog/src/test/java/com/chenliao/chenliaoblog/ChenliaoblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenliao.chenliaoblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChenliaoblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
